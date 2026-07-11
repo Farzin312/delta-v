f4c9cdca364ee8c9bf59fde7c6da10b991f0bf3c
Farzin Shifat <farzinshifat@gmail.com>
2026-07-11 19:28:43 -0400

    remove: delete unused example and module files for kinematics and vectors

--- first_30/practice_1/src/vectors.rs
+++
@@ -1,9 +0,0 @@
-// Module: vectors
-// Called from main.rs via vectors::run()
-
-pub fn run() {
-    let ax: f64 = 3.0;
-    let ay: f64 = 4.0;
-    let magnitude = (ax * ax + ay * ay).sqrt();
-    println!("({}, {}) -> magnitude = {}", ax, ay, magnitude);
-}
